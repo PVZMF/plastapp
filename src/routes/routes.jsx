8cffdb9186ad13cd9260af8a3371bdb696419140
Mohamad Hossein Moghaddamfar <mhmoghadamfar@gmail.com>
2022-11-15 11:26:58 +0330

    install react-router-dom

---
+++ src/routes/routes.jsx
@@ -0,0 +1,43 @@
+
+import { createBrowserRouter } from "react-router-dom";
+
+const DashboardRoutes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Dashboard />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/table",
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Table />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+    {
+      path: "/",
+      children: [
+        {
+          path: "/login",
+          element: (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Login />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ]);
+  
+  export default DashboardRoutes;
+  
