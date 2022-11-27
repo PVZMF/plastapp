99babe03e95bc03b31fda363e145d63a7c152f34
Mohamad Hossein Moghaddamfar <mhmoghadamfar@gmail.com>
2022-11-27 16:44:02 +0330

    config axios refresh Token

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,16 @@
 import axios from "axios";
+import Storage from "../service/Storage";
+
 export const baseUrl = "https://plastapp.iran.liara.run/";
 
-
+const st = Storage();
 const api = axios.create({
   baseURL: baseUrl,
-  timeout: 5000
+  timeout: "5000"
 });
+
+// api.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(st.accessToken())}`;
+// axios.defaults.headers.post['Authorization'] = `Bearer ${st.accessToken()}`;
 
 api.interceptors.request.use(
   function (config) {
@@ -26,6 +31,27 @@ api.interceptors.response.use(
     return response;
   },
   function (error) {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            return api('api/token/refresh/',
+                {
+                    "refresh_token": st.refreshToken()
+                })
+                .then(res => {
+                    if (res.status === 201) {
+                        // 1) put token to LocalStorage
+                        st.setLogin(res.data);
+
+                        // 2) Change Authorization header
+                        api.defaults.headers.common['Authorization'] = 'Bearer ' + st.accessToken;
+
+                        // 3) return originalRequest object with Axios.
+                        return api(originalRequest);
+                    }
+                })
+        }
+
     if (error.response.status === 400) {
       // try to get new access token with refresh token
       // try the request again
@@ -33,10 +59,11 @@ api.interceptors.response.use(
       // return error.response.data;
       return error.response;
     }
-
-    return Promise.reject(error);
   }
+  
 );
+
+
 
 export default api;
 
--- src/api/axiosLogin.js
+++ src/api/axiosLogin.js
@@ -5,9 +5,6 @@ export const baseUrl = "https://plastapp.iran.liara.run/";
 
 const st = Storage();
 const api = axios.create({
-    headers: {
-        Authorization : `Bearer ${st.accessToken}`
-        },
   baseURL: baseUrl,
   timeout: "5000"
 });
@@ -34,6 +31,27 @@ api.interceptors.response.use(
     return response;
   },
   function (error) {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            return api('api/token/refresh/',
+                {
+                    "refresh_token": st.refreshToken()
+                })
+                .then(res => {
+                    if (res.status === 201) {
+                        // 1) put token to LocalStorage
+                        st.setLogin(res.data);
+
+                        // 2) Change Authorization header
+                        api.defaults.headers.common['Authorization'] = 'Bearer ' + st.accessToken;
+
+                        // 3) return originalRequest object with Axios.
+                        return api(originalRequest);
+                    }
+                })
+        }
+
     if (error.response.status === 400) {
       // try to get new access token with refresh token
       // try the request again
@@ -41,10 +59,11 @@ api.interceptors.response.use(
       // return error.response.data;
       return error.response;
     }
-
-    return Promise.reject(error);
   }
+  
 );
+
+
 
 export default api;
 
