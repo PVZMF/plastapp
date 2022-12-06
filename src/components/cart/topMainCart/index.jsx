7144a8115981157599cf6dbf92613cb0bc096936
mrsase <36380619+mrsase@users.noreply.github.com>
2022-12-06 11:38:51 +0330

    card receipt naming bug fix

--- src/components/cart/topMainCart/index.jsx
+++ src/components/cart/topMainCart/index.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 // Components
-import Address from './address'
-import Baskets from './baskets'
-import BuyStep from './buyStep'
-import Receipt from './receipt/Index'
+import Address from "./address";
+import Baskets from "./baskets";
+import BuyStep from "./buyStep";
+import Receipt from "./receipt/index";
 
-import { FlexTopCart } from './styledTopCart'
+import { FlexTopCart } from "./styledTopCart";
 
 const TopMainCart = () => {
-
-  const [step, setStep] = useState(0)
-  const state = useSelector(state => state.cartState);
-  
+  const [step, setStep] = useState(0);
+  const state = useSelector((state) => state.cartState);
 
   return (
     <FlexTopCart>
-
-      <div className='baskets'>
+      <div className="baskets">
         {state.step === 0 && <Baskets />}
         {state.step === 1 && <Address step={step} setStep={setStep} />}
         {state.step === 2 && <BuyStep step={step} setStep={setStep} />}
       </div>
 
-      <div className='receipt'>
+      <div className="receipt">
         <Receipt step={step} setStep={setStep} />
       </div>
-
     </FlexTopCart>
-  )
-}
+  );
+};
 
-export default TopMainCart
+export default TopMainCart;
