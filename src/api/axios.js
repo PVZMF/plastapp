3d25a887171919f88fa4c621e43a10db128333ce
mrsase <36380619+mrsase@users.noreply.github.com>
2022-12-10 12:35:40 +0330

    token refresh fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Storage from "../service/Storage";
-
+import { refreshToken } from "./api";
 export const baseUrl = "https://plastapp.iran.liara.run/";
 
 const api = axios.create({
@@ -30,33 +30,15 @@ api.interceptors.response.use(
     return response;
   },
   function (error) {
-    // const originalRequest = error.config;
-    // if (error.response.status === 401 && !originalRequest._retry) {
-    //     originalRequest._retry = true;
-    //     return api('api/token/refresh/',
-    //         {
-    //             "refresh_token": st.refreshToken()
-    //         })
-    //         .then(res => {
-    //             if (res.status === 201) {
-    //                 // 1) put token to LocalStorage
-    //                 st.setLogin(res.data);
-
-    //                 // 2) Change Authorization header
-    //                 api.defaults.headers.common['Authorization'] = 'Bearer ' + st.accessToken;
-
-    //                 // 3) return originalRequest object with Axios.
-    //                 return api(originalRequest);
-    //             }
-    //         })
-    // }
-    // if (error.response.status === 400) {
-    // try to get new access token with refresh token
-    // try the request again
-    // logout user and redirect to login page if error
-    // return error.response.data;
-    // return error.response;
-    // }
+    if (error.response.status == 401) {
+      const st = Storage();
+      console.log({ refresh: st.refreshToken });
+      refreshToken({ refresh: st.refreshToken })
+        .then((res) => {
+          st.setAccessToken(res.access);
+        })
+        .catch((error) => console.log(error));
+    }
 
     return Promise.reject(error);
   }
