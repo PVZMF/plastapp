fe830f25ec320a1ad376036393ea42fd2109572d
mrsase <36380619+mrsase@users.noreply.github.com>
2022-12-11 09:38:21 +0330

    ticket detail page ui done

--- src/api/api.js
+++ src/api/api.js
@@ -125,6 +125,11 @@ export async function createTicket(ticketDetail) {
   return res.data;
 }
 
+export async function ticketDetail(id) {
+  const res = await apiLogin.get(`ticket/detail/${id}/`);
+  return res.data;
+}
+
 export async function addCheque(chequeDetail) {
   const res = await apiForm.post("order/check_create/", chequeDetail);
   return res.data;
