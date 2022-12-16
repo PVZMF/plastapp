87774d9927fb9873ecad975272149ad8a7162127
pouriya-chibaie-dev <85185561+pouriya-chibaie-dev@users.noreply.github.com>
2022-12-16 21:14:35 +0330

    compelete tesk searchbar in product page

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -3,19 +3,17 @@ import CardProduct from "./cardProduct";
 import { getCategories, getListProduct } from "../../api/api";
 // Style
 import style from "./products.module.css";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 const Products = () => {
-  const navigate=useNavigate()
-  const {id} = useParams();
-  console.log("params = " ,id)
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log("params = ", id);
   const [categorys, setCategorys] = useState([]);
   const [listProducts, setListProducts] = useState([]);
   const [filtterdlistProducts, setFiltterdlistProducts] = useState([]);
-
+  const [searchParamas, setSearchParams] = useSearchParams("");
   useEffect(() => {
-
-    
     //Products
     getListProduct()
       .then((res) => {
@@ -29,31 +27,43 @@ const Products = () => {
         setCategorys(results);
       })
       .finally(() => {});
-     
-
-    }, []);
-    useEffect(()=>{
-      const data=  listProducts.filter(item=>item.category.id ==parseInt(id))
-      setFiltterdlistProducts(data)
-    },[listProducts])
-    const changeCategory =  (id)=>{
-      const data=  listProducts.filter(item=>item.category.id ==parseInt(id))
-      setFiltterdlistProducts(data)
-      navigate(`/category/${id}/products`)
-    }
- 
-
+  }, []);
+  useEffect(() => {
+    const data = listProducts.filter(
+      (item) => item.category.id == parseInt(id)
+    );
+    setFiltterdlistProducts(data);
+  }, [listProducts]);
+  const changeCategory = (id) => {
+    const data = listProducts.filter(
+      (item) => item.category.id == parseInt(id)
+    );
+    setFiltterdlistProducts(data);
+    navigate(`/category/${id}/products`);
+  };
+useEffect(()=>{
+  console.log(searchParamas.get('filter'))
+},[searchParamas.get('filter')])
   return (
     <div className={style.products}>
       <div className={style.sidebar}>
         <div className={style.productsgroup}>
           <h5>دسته بندی محصولات</h5>
           <ul>
-          <li  onClick={()=>{changeCategory("all")}}>
-                <p>همه محصولات</p>
-              </li>
+            <li
+              onClick={() => {
+                changeCategory("all");
+              }}
+            >
+              <p>همه محصولات</p>
+            </li>
             {categorys.map((item, index) => (
-              <li key={index + "categorys"} onClick={()=>{changeCategory(item.id)}}>
+              <li
+                key={index + "categorys"}
+                onClick={() => {
+                  changeCategory(item.id);
+                }}
+              >
                 <p>{item.title}</p>
               </li>
             ))}
@@ -63,14 +73,30 @@ const Products = () => {
 
       <div className={style.productsBox}>
         <div className={style.searchbox}>
-          <input type="text" placeholder="جستجو محصول" />
+          <input
+            type="text"
+            value={searchParamas.get("filter") || ""}
+            placeholder="جستجو محصول"
+            onChange={(event) => {
+              let filter = event.target.value;
+              if (filter) {
+                setSearchParams({ filter });
+              } else {
+                setSearchParams({});
+              }
+            }}
+          />
         </div>
         <div className={style.group_categorys}>
-          <select onChange={(e)=>{changeCategory(e.target.value)}}>
-              <option >انتخاب دسته بندی</option>
-              <option value={"all"}>همه محصولات</option>
+          <select
+            onChange={(e) => {
+              changeCategory(e.target.value);
+            }}
+          >
+            <option>انتخاب دسته بندی</option>
+            <option value={"all"}>همه محصولات</option>
             {categorys.map((item, index) => (
-              <option key={index + "categorys"} value={item.id} >
+              <option key={index + "categorys"} value={item.id}>
                 {item.title}
               </option>
             ))}
@@ -79,26 +105,47 @@ const Products = () => {
 
         <div className={style.items}>
           {console.log(listProducts)}
-          {id ==="all" ?[...listProducts].reverse().map((item, index) => {
-            return (
-              <CardProduct
-                item={item}
-                key={index + "products"}
-                categorys={categorys}
-              />
-            );
-          } ) : "" }
-          {id !="all"?
-            [...filtterdlistProducts].reverse().map((item, index) => {
-              return (
-                <CardProduct
-                  item={item}
-                  key={index + "products"}
-                  categorys={categorys}
-                />
-              );
-            }  ):"" 
-          }
+          {id === "all" && searchParamas.get('filter') ==null
+            ? [...listProducts].reverse().map((item, index) => {
+                return (
+                  <CardProduct
+                    item={item}
+                    key={index + "products"}
+                    categorys={categorys}
+                  />
+                );
+              })
+            : ""}
+          {id != "all" && searchParamas.get('filter') ==null
+            ? [...filtterdlistProducts].reverse().map((item, index) => {
+                return (
+                  <CardProduct
+                    item={item}
+                    key={index + "products"}
+                    categorys={categorys}
+                  />
+                );
+              })
+            : ""}
+          {/*Show all products filtered by user search  */}
+          {listProducts
+            .filter((item) => {
+              let filter = searchParamas.get("filter");
+              if (!filter) return true;
+              let name = item.title.toLowerCase();
+              return name.startsWith(filter.toLowerCase());
+            })
+            .map((item, index) => {
+              if (searchParamas.get("filter")) {
+                return (
+                  <CardProduct
+                    item={item}
+                    key={index + "products"}
+                    categorys={categorys}
+                  />
+                );
+              }
+            })}
         </div>
       </div>
     </div>
