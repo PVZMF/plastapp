b5a07be141173e959ad951d33fc250bee72a44af
pouriya-chibaie-dev <85185561+pouriya-chibaie-dev@users.noreply.github.com>
2022-12-28 01:37:52 +0330

    compelete delete product task

--- src/components/profile/myProducts/index.jsx
+++ src/components/profile/myProducts/index.jsx
@@ -3,11 +3,35 @@ import { RiAddFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import style from "./myProducts.module.css";
 // import {listProduct} from "../../../api/api"
-import {discountCreate, discountDelete, discountUpdate, getListMyProduct } from "../../../api/api";
+import {deleteProduct, discountCreate, discountDelete, discountUpdate, getListMyProduct } from "../../../api/api";
 import {toast, ToastContainer } from "react-toastify";
 import { AxiosError, AxiosResponse } from "axios";
 import { useDispatch, useSelector } from "react-redux";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const MyProducts = () => {
+  
+    const [open, setOpen] = React.useState(false);
+    const [deleteState, setDeleteState] = React.useState("");
+  
+    const handleClickOpen = (id,title)=> {
+      setDeleteState({id ,title})
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
   const [off, setOff] = useState(0);
   const [listProducts, setListProducts] = useState([]);
   const [update,setUpdate] = useState(false)
@@ -55,7 +79,16 @@ const MyProducts = () => {
     })
   }
 
-
+ const deleteProductHandler=(id) =>{
+deleteProduct(id).then((res)=>{
+  setUpdate(!update)
+toast.success("با موفقیت حذف شد")
+setOpen(false)
+})
+.catch((err)=>{
+  toast.error("محصول حذف نشد")
+})
+ }
 //Create Or Edit Discount 
   const changeDiscountValue = (idP,idD) => {
     
@@ -106,6 +139,9 @@ const MyProducts = () => {
                 <h4>محصول</h4>
               </th>
               <th>
+                <h4>حذف محصول</h4>
+              </th>
+              <th>
                 <h4>محدوده ارسال</h4>
               </th>
               <th>
@@ -128,6 +164,9 @@ const MyProducts = () => {
                 <tr key={item.id}>
                   <td>
                     <h3>{item.title}</h3>
+                  </td>
+                  <td>
+                    <Button variant="contained" sx={{width:"100%"}} color="error" onClick={()=>{handleClickOpen(item.id,item.title)}}>حذف محصول</Button>
                   </td>
                   <td>
                     <h3>{item.send_to_all_point ? "سرار کشور" : ""}</h3>
@@ -209,6 +248,27 @@ draggable
 pauseOnHover
 theme="colored"
 />
+<div>
+      <Dialog 
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle sx={{minWidth:"300px" ,fontSize:"20px",width:"100%"}}>آیا از حذف محصول <span style={{color:"#3C76D2"}}>{deleteState.title}</span> مطمئن  هستید؟
+</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="error"onClick={handleClose} sx={{marginLeft:"20px"}}>خیر</Button>
+          <Button variant="contained" onClick={()=>deleteProductHandler(deleteState.id)}>بله</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    <ToastContainer/>
     </div>
   );
 };
